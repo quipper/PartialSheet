5a5a5a28a9e88d3db1cda7bfe6d30736a78e3656
komaji <komaji@users.noreply.github.com>
2023-09-06 11:27:59 +0900

    Fix empty sheet on ipad

--- Sources/PartialSheet/PartialSheetManager.swift
+++ Sources/PartialSheet/PartialSheetManager.swift
@@ -28,6 +28,7 @@ public class PartialSheetManager: ObservableObject {
         didSet {
             if !isPresented {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) { [weak self] in
+                    if self?.isPresented ?? true { return }
                     self?.content = AnyView(EmptyView())
                     self?.onDismiss = nil
                 }
